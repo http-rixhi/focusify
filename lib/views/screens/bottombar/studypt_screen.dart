eeccba388fccd99271f6223d3bf568e9cd2011a6
Rishi <rishiraj11a@gmail.com>
2024-07-18 13:05:36 +0530

    Update Generative AI for doubt solving.

--- lib/views/screens/bottombar/studypt_screen.dart
+++ lib/views/screens/bottombar/studypt_screen.dart
@@ -1,10 +1,13 @@
 // BSD 3-Clause License
 // Copyright (c) 2023, Rishi Raj & Pushpendra Baswal
 
+// import 'dart:nativewrappers/_internal/vm/lib/core_patch.dart';
+
 import 'package:flutter/material.dart';
+import 'package:flutter_markdown/flutter_markdown.dart';
 import 'package:fluttericon/font_awesome5_icons.dart';
 import 'package:google_generative_ai/google_generative_ai.dart';
-import 'package:velocity_x/velocity_x.dart';
+import 'package:markdown/markdown.dart' as md;
 
 class studyAI extends StatefulWidget {
   const studyAI({super.key});
@@ -22,18 +25,30 @@ class _studyAIState extends State<studyAI> {
     apiKey: 'AIzaSyBSwSQ0NQ_bNiP9LlsKqyES6YATYaEBtdA',
   );
 
-
-
   @override
   Widget build(BuildContext context) {
-
+    String markdownText = _data.toString();
+    String plainText = markdownToPlainText(markdownText);
     if(_data == null) {
-      _data = "Heyyy, I'm your doubt assistant. Ask me anything...";
+      _data = "Heyyy, I'm your doubt assistant. Ask me anything...\n"
+          """
+                 _______
+               _/       \\_
+              / |       | \\
+             /  |__   __|  \\
+           |__/((o| |o))\\__|
+            |      | |      |
+            |\\     |_|     /|
+            | \\           / |
+             \\| /   _   \\ |/
+              \\ | \\___/ | /
+               \\_________/
+               _|_____|_
+           ____|_________|____
+          /                   \\ 
+          """;
     }
 
-    // var markdown = """
-    // SJDFKAJ sjnds *fdsfds* **sdfdsfsdf**
-    // """;
     return Scaffold(
       body: Column(
         mainAxisAlignment: MainAxisAlignment.end,
@@ -42,16 +57,19 @@ class _studyAIState extends State<studyAI> {
             child: SingleChildScrollView(
               child: Padding(
                 padding: const EdgeInsets.symmetric(horizontal: 5),
-                child: Container(
-                  decoration: BoxDecoration(
-                    border: Border.all(color: Colors.white38),
-                  ),
-                  child: Padding(
-                    padding: const EdgeInsets.symmetric(
-                      horizontal: 15,
-                      vertical: 8
+                child: SizedBox(
+                  height: 500,
+                  child: Container(
+                    decoration: BoxDecoration(
+                      border: Border.all(color: Colors.white38),
                     ),
-                    child: Text(_data.toString()),
+                    child: Padding(
+                      padding: const EdgeInsets.symmetric(
+                        horizontal: 15,
+                        vertical: 8
+                      ),
+                      child: Markdown(data: _data)
+                    ),
                   ),
                 ),
               ),
@@ -69,7 +87,6 @@ class _studyAIState extends State<studyAI> {
             trailing: IconButton(onPressed: () {
               setState(() {
                 _data = "Searching for your query...";
-
               });
               GetResults();
             }, icon: Icon(Icons.send)),
@@ -86,5 +103,11 @@ class _studyAIState extends State<studyAI> {
     setState(() {
       _data = response.text;
     });
+  }
+
+  String markdownToPlainText(String markdown) {
+    final document = md.Document();
+    final parsed = document.parseInline(markdown);
+    return parsed.map((e) => e.textContent).join();
   }
 }
